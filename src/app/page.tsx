bb21987e99ff5ebe92ef8d6484812c217fcf5a43
Himanshu Bhargava <54812738+hb195@users.noreply.github.com>
2025-02-10 17:53:19 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Did it work?? Get started by editing <code>src/app/page.tsx</code>.
+            Did it work??????? Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
